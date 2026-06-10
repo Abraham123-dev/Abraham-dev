396f47fa85873e078d5a199dd7827b82e3bf244d
Abraham123-dev <abrahamoluwaniyi50@gmail.com>
2026-06-11 00:47:34 +0100

    IMPORTED CORRECTLY

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { FadeIn } from "./FadeIn";
 import { PageTransition } from "./PageTransition";
 import voxyImg from "../assets/voxy.png";
-import cdssImg from "../assets/cdss.png";
+import cdssImg from "../assets/CDSS.png";
 import adaImg from "../assets/ada.png";
 import tictactoeImg from "../assets/tictactoe.png";
 
